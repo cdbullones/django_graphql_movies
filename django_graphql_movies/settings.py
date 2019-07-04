eabd0b73b229bafe7cfb5e371612a12bf538ccdd
cdbullones <cdbullones@gmail.com>
2019-07-04 16:30:02 -0400

    new packages in requirements

--- django_graphql_movies/settings.py
+++ django_graphql_movies/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'graphene_django',
     'movies',
 ]
@@ -53,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'django_graphql_movies.urls'
 
